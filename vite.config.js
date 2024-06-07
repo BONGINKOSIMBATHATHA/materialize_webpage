14da9d4dd07cab0fc1f2e025b6c447008586db4c
BONGINKOSIMBATHATHA <bonginkosimbatha09@gmail.com>
2024-06-07 09:48:42 +0200

    Eugene

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from "./New folder/data.json";
+export default {
+  plugins: [handlebars({
+    context : {
+        /*
+        previous setup here
+        */
+        data   // add this
+    }
+  })],
+};
