ecfc42a6b2b8b36e64eaa83ced4471fa9bf131ec
BONGINKOSIMBATHATHA <bonginkosimbatha09@gmail.com>
2024-06-07 10:38:02 +0200

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import handlebars from 'vite-plugin-handlebars';
 import data from "./New folder/data.json";
 export default {
   plugins: [handlebars({
-    context : {
+    context : 
         /*
         previous setup here
         */
         data   // add this
-    }
+    
   })],
 };
